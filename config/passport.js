4ba4506158b3280bf02eb34f1867795126be3665
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-05-02 09:36:48 -0700

    Seperated api

--- config/passport.js
+++
@@ -1,9 +0,0 @@
-var passport = require("passport");
-const Auth = require("../models/auth");
-const LocalStrategy = require("passport-local").Strategy;
-
-passport.use(new LocalStrategy(Auth.authenticate()));
-passport.serializeUser(Auth.serializeUser());
-passport.deserializeUser(Auth.deserializeUser());
-
-module.exports = passport;
--- models/auth.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
-
-const Auth = new Schema({ username: String, password: String });
-
-Auth.plugin(passportLocalMongoose);
-
-module.exports = mongoose.model("Auth", Auth);
