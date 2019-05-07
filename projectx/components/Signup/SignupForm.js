ef4bfe3e7b284150fbe1ea40ecbcf7e3e7669683
nkanderson86 <N.ANDERSON.398@GMAIL.COM>
2019-05-07 06:41:54 -0700

    added API calls and some methods to the SignupForm file

--- projectx/components/Signup/SignupForm.js
+++ projectx/components/Signup/SignupForm.js
@@ -1,12 +1,31 @@
 // required imports and dependencies
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { Form, Item, Input, Label } from 'native-base';
 import CreateAccountButton from "./CreateAccountButton"
 import { withNavigation } from 'react-navigation';
 
 // create classful component
 class SignupForm extends Component {
+
+    state = {
+        username: '',
+        password: '',
+        deviceId: ''
+
+    }
+
+    generateDeviceId = () => {
+        let id = Math.floor(Math.random() * (10000) + 1000)
+        console.log("ID " + id)
+        this.setState({ deviceId: id })
+        console.log("set state: " + this.state.deviceId)
+    }
+
+    componentDidMount() {
+        this.generateDeviceId();
+        console.log("working" + this.state.deviceId)
+    }
 
     render() {
 
@@ -15,15 +34,15 @@ class SignupForm extends Component {
             <Form style={styles.signupContainer}>
                 <Item style={styles.signupField} floatingLabel last>
                     <Label>Device ID</Label>
-                    <Input />
+                    {/* <Text> {this.state.deviceId} </Text> */}
                 </Item>
                 <Item style={styles.signupField} floatingLabel last>
                     <Label>Set Username</Label>
-                    <Input />
+                    <Input onChangeText={(value) => this.setState({ username: value })} />
                 </Item>
                 <Item style={styles.signupField} floatingLabel last>
                     <Label>Set Password</Label>
-                    <Input />
+                    <Input secureTextEntry={true} onChangeText={(value) => this.setState({ password: value })} />
                 </Item>
                 {/* button to route user, input logic to create account on component CreateAccountButton.js */}
                 <CreateAccountButton />
