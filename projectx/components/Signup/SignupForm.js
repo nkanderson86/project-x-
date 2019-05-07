ef11901e3ca75b2595a2311f0cdafbd9c38ecdcc
nkanderson86 <N.ANDERSON.398@GMAIL.COM>
2019-05-07 14:16:05 -0700

    committing before pulling from develop branch

--- projectx/components/Signup/SignupForm.js
+++ projectx/components/Signup/SignupForm.js
@@ -55,7 +55,7 @@ class SignupForm extends Component {
             <Form style={styles.signupContainer}>
                 <Item style={styles.signupField} floatingLabel last>
                     <Label>Device ID</Label>
-                    <Text> {this.state.id} </Text>
+                    <Text> {$(this.state.id)} </Text>
                 </Item>
                 <Item style={styles.signupField} floatingLabel last>
                     <Label>Set Username</Label>
